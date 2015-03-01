767e26188ac843a421c57de2e418c8295667ef67
101316101 <greenfish0626@gamil.com>
2015-03-01 17:55:37 +0800

    week1
    week1

---
+++ Homework.java
@@ -0,0 +1,9 @@
+class Homework {
+	public static void main(String[] args) {
+ 		System.out.println("\t〈渭城曲〉王維");
+		System.out.println("\t渭城朝雨浥輕塵");
+		System.out.println("\t客舍青青柳色新");
+		System.out.println("\t勸君更盡一杯酒");
+		System.out.println("\t西出陽關無故人");
+	}
+}
